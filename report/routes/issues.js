9df350b3d3c98e95df619c35fff39d49a104eaab
Ognian Tschakalov <ognian.tschakalov@ogi-it.com>
2013-11-11 12:50:20 +0100

    - ignoring printtest since this is pure generated output with prince - removed gravatars since in huge files remote requests are far too much...

--- report/routes/issues.js
+++ report/routes/issues.js
@@ -11,7 +11,8 @@ exports.report = function (req, res) {
     var filename = path.join(conf.path, req.params.filename);
 
     // for now res render is pure in memory; this could be a problem; actually therefor we have chosen dust since it could be the only one templating engine supporting async reendering...
-    //TODO make this async and in small chunks, that is why I ve chosen dust js!!
+    // TODO make this async and in small chunks, that is why I ve chosen dust js!! --> not yet browsers are pretty busy when we have here 20mb or so of data resulting in 5k+ pages...
+    // 
 
     var hugeArray = [];
 

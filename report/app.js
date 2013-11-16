41650e8b47d8611049028e90de5b1608186b8a65
Ognian Tschakalov <ognian.tschakalov@ogi-it.com>
2013-11-16 13:23:45 +0100

    - intermediate

--- report/app.js
+++ report/app.js
@@ -32,21 +32,96 @@ dust.helpers.formatDate = function (chunk, context, bodies, params) {
     date = timestamp.getDate();
     year = timestamp.getFullYear();
 
-    return chunk.write(date + '.' + month + '.' + year);
+    return chunk.write(date + '.' + month + '.' + year); //TODO why do we have here write and not end ?????
 };
-var marked = require('marked');
 
-dust.helpers.markdown = function(chunk, context, bodies, params) {
+
+var marked = require('marked');
+marked.setOptions({
+    gfm: true,
+//    highlight: function (code, lang, callback) {
+//        pygmentize({ lang: lang, format: 'html' }, code, function (err, result) {
+//            if (err) return callback(err);
+//            callback(null, result.toString());
+//        });
+//    },
+    tables: true,
+    breaks: false,
+    pedantic: true,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false,
+    langPrefix: 'lang-'
+});
+
+dust.helpers.marked = function (chunk, context, bodies, params) {
     if (bodies.block) {
-        return chunk.capture(bodies.block, context, function(string, chunk) {
+        return chunk.capture(bodies.block, context, function (string, t_chunk) {   //TODO check why we have here a chunk.capture
             //console.log('.');
-            //try{
-            chunk.end(marked(string));
-            //} catch (err){console.log("marked error: "+err);}
+
+// var. 1 sync
+//            var str;
+//            try {
+//                //console.log("STRING");
+//                //console.log(string);
+//                str = marked(string);
+//                t_chunk.end(str);
+//                //console.log("STR");
+//                //console.log(str);
+//
+//            } catch (err) {
+//                str = "MARKED ERROR START:" + err + "MARKED ERROR END."
+//                console.log(str);
+//                t_chunk.end(str);
+//            }
+
+// var. 2 async
+                marked(string, function (err, content) {
+                    if (err) {
+                    console.log("marked async ERROR: "+err);
+                    }
+//                    console.log("CONTEND");
+//                    console.log(content);
+                    //jgfsdlgflas //errors here are ignored!!!! thats the problem !!!!
+                    t_chunk.end(content);
+                });
+
+
         });
     }
     return chunk;
 };
+
+var Showdown = require('showdown');
+var converter = new Showdown.converter({ extensions: ['github', 'prettify', 'table'] });
+dust.helpers.markdown = function (chunk, context, bodies, params) {
+    if (bodies.block) {
+        return chunk.capture(bodies.block, context, function (string, chunk) {
+            //console.log('.');
+            var str;
+            try {
+                //console.log("STRING");
+                //console.log(string);
+
+                str = converter.makeHtml(string);
+                chunk.end(str);
+                //console.log("STR");
+                //console.log(str);
+            } catch (err) {
+//                console.log("STRING");
+//                console.log(string);
+//                console.log("STR");
+//                console.log(str);
+
+                str = "MARKDOWN ERROR START:" + err + "MARKDOWN ERROR END."
+                console.log(str);
+                chunk.end(str);
+            }
+        });
+    }
+    return chunk;
+};
+
 
 var cons = require('consolidate');
 
@@ -60,6 +135,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 
+//app.use(express.compress()); //not a good idea; not chunked...
+
 app.use(express.favicon()); //TODO change to custom one
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -70,13 +147,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 app.disable('x-powered-by'); //get rid of the x-powerd-by = express header...
 
 app.get('/', routes.index);
 app.get('/issues/:filename', issues.report);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- report/routes/issues.js
+++ report/routes/issues.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 var conf = require('../config.json');
 var path = require('path');
+var util = require('util');
 
-//var dust = require('dustjs-linkedin');
+var dust = require('dustjs-linkedin');
+dust.isDebug=true;
 
 /*
  * GET issues listing.
@@ -25,9 +27,9 @@ exports.report = function (req, res) {
     });
 
     console.log("start rendering");
-    res.render('issues', { name: req.params.filename,
-        allDocs: hugeArray
-    });
+//    res.render('issues', { name: req.params.filename,
+//        allDocs: hugeArray
+//    });
 
     // What does render? going thrue res app -> view.render :
     // this.engine(this.path, options, fn);
@@ -42,31 +44,55 @@ exports.report = function (req, res) {
     //  or compile template 'issues'
     //  and call it...
 
-//    var tmplName = "issues";
-//    var tmplString = fs.readFileSync("views/"+tmplName+".dust").toString(); //this is usually no problem since the template is quite short...
-//
-//    var count=0;
-//
-//    var compiled = dust.compile(tmplString, tmplName); //template string, tmpl name to be set in loadSource
-//    dust.loadSource(compiled); //caching and setting name
-//    res.writeHead(200, {'Content-Type': 'text/html'});
-//    dust.stream(tmplName, { name: req.params.filename,
-//        allDocs: hugeArray
-//    })//tmpl name, context
-//        .on('data', function (chunk) {
-//            if (chunk) {
-//                //console.log('#');
-//                count++;
-//                if(count%1000==0)console.log(count);
-//                res.write(chunk,'utf8');  //this is a node function!! (also end etc...)
-//            }
-//        })
-//        .on('end', function () {
-//            res.end();
-//        })
-//        .on('error', function (err) {
-//            res.write("ERRORORORORORORORO " + err);
-//            res.end();
-//        });
+    var tmplName = "issues";
+    var tmplString = fs.readFileSync("views/" + tmplName + ".dust").toString(); //this is usually no problem since the template is quite short...
+
+    var count = 0;
+
+    var compiled = dust.compile(tmplString, tmplName); //template string, tmpl name to be set in loadSource
+    dust.loadSource(compiled); //caching and setting name
+    //res.writeHead(200, {'Content-Type': 'text/html'}); //better without???
+
+    //res.socket.setNoDelay();
+    // to late to set it here ..... res.socket.bufferSize=102400;
+
+    var dstr = dust.stream(tmplName, { name: req.params.filename,
+        allDocs: hugeArray
+    });//tmpl name, context
+
+// var. 1 this is the simplest piping way to do it
+//    dstr.pipe(res);
+
+// var. 2 this is more complicated but I have grater control i.e. progress output...
+    dstr.on('data', function (chunk) {
+        if (chunk) {
+//           process.nextTick(function () {
+                //console.log('#');
+                count++;
+                if (count % 1000 == 0) {
+                    console.log(count);
+                    console.log(util.inspect(process.memoryUsage()));
+                }
+                var result = res.write(chunk);  //this is a node function!! (also end etc...)
+                if (!result) {
+                    //dstr.pause(); // dust stream has no pause...
+                    //console.log("Problem flushing buffer to kernel: result: " + result);
+                }
+//            }.bind(this));
+        }
+    });
+    dstr.on('end', function () {
+        console.log('END');
+        res.end();
+    });
+    dstr.on('error', function (err) {
+        res.write("ERRORORORORORORORO " + err);
+        res.end();
+    });
+
+//    res.on('drain', function() {
+//        // Resume the read stream when the write stream gets hungry
+//        dstr.resume();
+//    });
 
 };
